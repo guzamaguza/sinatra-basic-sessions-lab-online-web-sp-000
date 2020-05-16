867713d581da0bb55eee8a32f5c23367f2133421
Mark A Guza <guzamaguza@gmail.com>
2020-05-16 04:52:32 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -14,7 +14,7 @@ class App < Sinatra::Base
   post '/checkout' do
     item = params[:item]
     session["item"] = item
-    @session = session.value.first
+    @session = session["item"]
 
     erb :display_item
   end
